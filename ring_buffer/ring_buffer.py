71202f7fd587dffc196ac08993a1bfdb0bb769e3
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-07-17 11:21:17 -0400

    Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.info = []
+        self.oldest = 0
 
     def append(self, item):
-        pass
+        if len(self.info) < self.capacity:
+            self.info.append(item)
+        else:
+            self.info[self.oldest] = item
+            self.oldest += 1
+        if self.oldest == self.capacity:
+            self.oldest = 0
 
     def get(self):
-        pass
+        return self.info
